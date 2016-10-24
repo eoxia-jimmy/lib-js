5c774d2775a00cfb468d6adfdc52c0a9b4222cd5
Jimmy <lelabodudev@gmail.com>
2016-10-24 10:20:34 +0200

    MAJ

--- examples/request-delete/exemple.app.js
+++ examples/request-delete/exemple.app.js
@@ -1,6 +1,5 @@
 window.app.request_delete = {};
 
 window.app.request_delete.delete_line = function( element, response ) {
-	console.log(element);
   jQuery( element ).closest( 'ul' ).remove();
-}
+};
